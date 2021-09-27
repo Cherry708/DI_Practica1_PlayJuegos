69d4e94020e83a9e7e4212e51a19226e997f9621
Cherry708 <aluser8241@ieselcaminas.org>
2021-09-27 20:46:31 +0200

    Progreso

--- app/src/main/java/com/example/di_practica1_playjuegos/MainActivity.kt
+++ app/src/main/java/com/example/di_practica1_playjuegos/MainActivity.kt
@@ -3,7 +3,12 @@ package com.example.di_practica1_playjuegos
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.core.content.res.ResourcesCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,10 +19,33 @@ class MainActivity : AppCompatActivity() {
         val btnNewPlayer = findViewById(R.id.btnNewPlayer) as Button
         //Añadimos listener a ese objeto button y le suministramos la actividad que llevara a cabo
         btnNewPlayer.setOnClickListener { lanzaNewPlayer() }
+        //Cambiamos la fuente del titulo
+        val tvTitle = findViewById<TextView>(R.id.tvTitle)
+        tvTitle.typeface = ResourcesCompat.getFont(this, R.font.courgetteregular)
+
+
     }
     fun lanzaNewPlayer(){
         //val i = Intent(this, NewPlayer::class.java)
         //Buscar Intent, debe ser lanzar la nueva actividad¿
         startActivity(Intent(this, NewPlayer::class.java))
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.getItemId()) {
+            (R.id.action_search) -> {
+                Toast.makeText(this,"Search",Toast.LENGTH_LONG).show()
+                return true
+            }
+            (R.id.action_add) -> {
+                Toast.makeText(this,"Add",Toast.LENGTH_LONG).show()
+                return true
+            }
+            else -> {return super.onOptionsItemSelected(item)} }
     }
 }
