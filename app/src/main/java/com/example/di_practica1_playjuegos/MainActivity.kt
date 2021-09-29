cc800ffa49c9616e2951b21530f9d1b656fae47a
Cherry708 <aluser8241@ieselcaminas.org>
2021-09-29 20:34:32 +0200

    Progreso

--- app/src/main/java/com/example/di_practica1_playjuegos/MainActivity.kt
+++ app/src/main/java/com/example/di_practica1_playjuegos/MainActivity.kt
@@ -8,23 +8,36 @@ import android.view.MenuItem
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.widget.Toolbar
 import androidx.core.content.res.ResourcesCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         //Busacmos y asignamos el view/componente a un objeto(inferido como button)
         //btnNewPlayer en lugar de jugador, es buena practica?
         val btnNewPlayer = findViewById(R.id.btnNewPlayer) as Button
+
         //Añadimos listener a ese objeto button y le suministramos la actividad que llevara a cabo
         btnNewPlayer.setOnClickListener { lanzaNewPlayer() }
+
         //Cambiamos la fuente del titulo
         val tvTitle = findViewById<TextView>(R.id.tvTitle)
         tvTitle.typeface = ResourcesCompat.getFont(this, R.font.courgetteregular)
 
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+
 
     }
+
+    fun lanzaGeneros(){
+        startActivity(Intent(this,Generos::class.java))
+    }
+
     fun lanzaNewPlayer(){
         //val i = Intent(this, NewPlayer::class.java)
         //Buscar Intent, debe ser lanzar la nueva actividad¿
@@ -46,6 +59,13 @@ class MainActivity : AppCompatActivity() {
                 Toast.makeText(this,"Add",Toast.LENGTH_LONG).show()
                 return true
             }
-            else -> {return super.onOptionsItemSelected(item)} }
+            (R.id.action_settings) -> {
+                lanzaGeneros()
+                return true
+            }
+            else -> {return super.onOptionsItemSelected(item)}
+
+        }
+
     }
 }
